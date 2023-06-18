cd3abff7df29fa4d8234c0abe7bb2970a4306198
Mohammad Javidi <m.javidii@yahoo.com>
2023-06-18 15:20:35 +0330

    review base .go

--- web/controller/base.go
+++ web/controller/base.go
@@ -1,10 +1,9 @@
 package controller
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"xray-ui/web/session"
-
-	"github.com/gin-gonic/gin"
 )
 
 type BaseController struct {
@@ -13,7 +12,7 @@ type BaseController struct {
 func (a *BaseController) checkLogin(c *gin.Context) {
 	if !session.IsLogin(c) {
 		if isAjax(c) {
-			pureJsonMsg(c, false, "The time limit for logging in has expired, please log in again")
+			pureJsonMsg(c, false, "The time limit for logging in has expired，please log in again")
 		} else {
 			c.Redirect(http.StatusTemporaryRedirect, c.GetString("base_path"))
 		}
